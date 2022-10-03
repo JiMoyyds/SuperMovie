ab61a2a846a5f2275538123f8ee0463ce8fec5fb
Thaumy <thaumy@outlook.com>
2022-10-03 14:35:39 +0800

    fix db & fix film interface

--- SuperMovie_frame/SuperMovie_frame/src/film.cs
+++ SuperMovie_frame/SuperMovie_frame/src/film.cs
@@ -6,7 +6,7 @@ public interface IFilmModel
     public long Name { get; set; }
     public DateTime OnlineTime { get; set; }
     public bool IsPreorder { get; set; }
-    public string Types { get; set; }
+    public List<string> Types { get; set; }
     public double Price { get; set; }
     public List<string> Actors { get; set; }
 }
