88cf9f3b0193ce65e058395823042c43cd864c1b
Thaumy <thaumy@outlook.com>
2022-10-04 22:04:17 +0800

    Update model.cs

--- SuperMovie_frame/SuperMovie_frame/src/order/model.cs
+++ SuperMovie_frame/SuperMovie_frame/src/order/model.cs
@@ -5,7 +5,7 @@ public interface IOrderModel
 {
     public long Id { get; set; }
     public long UserId { get; set; }
-    public long PayAmount { get; set; }
+    public double PayAmount { get; set; }
     public DateTime Time { get; set; }
     public long FilmId { get; set; }
     public long CinemaId { get; set; }
