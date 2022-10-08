d1fe1754de448dfd419f396b84ceb4d202731673
Thaumy <thaumy@outlook.com>
2022-10-08 16:15:19 +0800

    update inerface

--- SuperMovie_frame/SuperMovie_frame/src/container/cinema/entity.cs
+++ SuperMovie_frame/SuperMovie_frame/src/container/cinema/entity.cs
@@ -25,7 +25,5 @@ public interface ICinemaEntity
     public string? Name { get; set; }
 
     public List<IScheduleEntity> Schedules { get; }
-    public bool AddSchedule(IScheduleEntity schedule);
-    public bool RemoveSchedule(IScheduleEntity schedule);
     public bool ClearSchedule();
 }
